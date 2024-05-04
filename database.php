2ea2e37bd9be0d732e68b898808d423ff4d4213c
HamdadCH <168775489+HamdadCH@users.noreply.github.com>
2024-05-04 19:50:38 +0200

    Form login and signup php/mysql/html

---
+++ database.php
@@ -0,0 +1,16 @@
+<?php
+$host = "localhost";
+$dbname = "login_db";
+$username = "root";
+$password = "";
+
+$mysqli = new mysqli(hostname: $host,
+					username: $username,
+					password: $password,
+					database: $dbname);
+
+if ($mysqli->connect_errno) {
+	die("connect errno: " . $mysqli->connect_errno);
+}
+return $mysqli;
+?>
